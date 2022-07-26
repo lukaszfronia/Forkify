e11200cc5e9eeaf29b6d7a23dddf356e8864f6e5
lukaszfronia <86720750+lukaszfronia@users.noreply.github.com>
2022-07-26 17:29:23 +0200

    Initial commit

---
+++ src/js/controller.js
@@ -0,0 +1,32 @@
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+const recipeContainer = document.querySelector(".recipe");
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+    // 1. Loading recipe
+    await model.loadREcipe(id);
+
+    // 2. Rendering recipe
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+controlRecipes();
+["hashchange", "load"].forEach((el) =>
+  window.addEventListener(el, controlRecipes)
+);
