fde2773e9d69b839e7b6ffa7186cf41286e91be4
lukaszfronia <86720750+lukaszfronia@users.noreply.github.com>
2022-08-31 15:18:10 +0200

    create documentation and update package.json

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,6 +2,15 @@ import icons from '../../img/icons.svg';
 
 export default class View {
   _data;
+  /**
+   * Render the recived object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Łukasz Fronia
+   * @todo Finish the implementation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
